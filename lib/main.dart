f59230de6bec95ee4e4d68a2b8e225de614ece61
Mr.Mk <kankrechamayank@gmail.com>
2022-07-21 05:06:48 +0530

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,27 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(Myapp());
 }
-class Myapp extends StatelessWidget {
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
 
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  var num=0,two=0,three=0;
+  void fun(){
+    // num = num + 1;
+    setState(() {
+      num = num + 1;
+      two = num * num;
+      three = two * two;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(appBar: AppBar(title: Text('My App'),),
         body: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -15,17 +32,23 @@ class Myapp extends StatelessWidget {
               children: [
                 Container(
                   padding: EdgeInsets.all(20),
-                  child: Text('one'),
+                  child: Text('$num',style: TextStyle(
+                    fontSize: 20,
+                  ),),
                   color: Colors.red,
                 ),
                 Container(
                   padding: EdgeInsets.all(40),
-                  child: Text('two'),
+                  child: Text('$two',style: TextStyle(
+                    fontSize: 20,
+                  ),),
                   color: Colors.yellow,
                 ),
                 Container(
                   padding: EdgeInsets.all(60),
-                  child: Text('tree'),
+                  child: Text('$three',style: TextStyle(
+                    fontSize: 20,
+                  ),),
                   color: Colors.green,
                 ),
               ],
@@ -37,17 +60,23 @@ class Myapp extends StatelessWidget {
               children: [
                 Container(
                   padding: EdgeInsets.all(20),
-                  child: Text('one'),
+                  child: Text('$num',style: TextStyle(
+                    fontSize: 20,
+                  )),
                   color: Colors.red,
                 ),
                 Container(
                   padding: EdgeInsets.all(40),
-                  child: Text('two'),
+                  child: Text('$two',style: TextStyle(
+                    fontSize: 20,
+                  )),
                   color: Colors.yellow,
                 ),
                 Container(
                   padding: EdgeInsets.all(60),
-                  child: Text('tree'),
+                  child: Text('$three',style: TextStyle(
+                    fontSize: 20,
+                  )),
                   color: Colors.green,
                 ),
                 Divider(color: Colors.black, height: 1,),
@@ -55,7 +84,15 @@ class Myapp extends StatelessWidget {
             )
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            fun();
+          },
+          child: Text("$num"),
+        ),
       ),
     );
   }
 }
+
+
